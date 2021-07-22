f1beeb3896c8241f33d69b6b498735d516cb6999
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2021-07-22 08:18:40 +0100

    complete exercise 5

--- exercises/testing/test.js
+++ exercises/testing/test.js
@@ -1,4 +1,12 @@
+const { findUser } = require("./users");
+
 // write some tests
-describe('users', () => {
-  
-})
+describe("users", () => {
+  test("should ", async () => {
+    expect(await findUser(1)).toEqual({
+      id: 1,
+      createdAt: Date.now() + 1,
+      email: "readycoder1@gmail.com",
+    });
+  });
+});
